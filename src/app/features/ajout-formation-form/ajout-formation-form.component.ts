5c4335ea4c2693cc9797d7e68a6d218f2ad36ade
julien <julien.prodhom@gmail.com>
2021-03-15 10:10:20 +0100

    améliorations graphiques

--- src/app/features/ajout-formation-form/ajout-formation-form.component.ts
+++ src/app/features/ajout-formation-form/ajout-formation-form.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { Formation } from 'src/app/models/formation';
 import { FormationNew } from 'src/app/models/FormationNew';
 import { FormationHttpService } from 'src/app/services/formation-http.service';
+import { DialogConfirmationAjoutComponent } from '../dialog-confirmation-ajout/dialog-confirmation-ajout.component';
 
 @Component({
   selector: 'app-ajout-formation-form',
@@ -12,9 +15,11 @@ export class AjoutFormationFormComponent implements OnInit {
 
   creationFormationForm: FormGroup;
   formationNew: FormationNew;
+  formation:Formation;
 
   constructor(
     private fb: FormBuilder,
+    public dialog: MatDialog,
     private formationService: FormationHttpService) {
     this.creationFormationForm = this.fb.group({
       titre: [''],
@@ -35,8 +40,14 @@ export class AjoutFormationFormComponent implements OnInit {
 
       this.formationService.save(this.formationNew).subscribe(res => {
           console.log(res);
+          if(this.formation != null) {
+            const dialogConfirm = this.dialog.open(DialogConfirmationAjoutComponent);
+            dialogConfirm.afterClosed().subscribe();
+          }
       });
     
   }
+
+ 
 
 }
